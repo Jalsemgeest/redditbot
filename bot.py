d4a2bdea727c73ffdc7dc7108d3201ced3899118
Jake Alsemgeest <alsemgeester@gmail.com>
2022-08-19 18:25:44 -0400

    Searching for stuff. Going to add some good stuff to it next.

--- bot.py
+++ bot.py
@@ -2,7 +2,11 @@ import praw
 
 reddit = praw.Reddit('JakesBot')
 
-subreddit = reddit.subreddit('learnpython')
+subreddit = reddit.subreddit('pythonforengineers')
 
-for submission in subreddit.hot(limit=5):
-	print("Title: ", submission.title)
+# for submission in subreddit.hot(limit=5):
+# 	print("Title: ", submission.title)
+
+# This will block and continually fetch new comments within a thread.
+for comment in subreddit.stream.comments():
+    print(comment.body)
